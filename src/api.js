57dcfb5354a44e98d258df98c491eb6c25ebf86a
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2023-03-31 10:49:16 +0200

    Added search bar with GeoDB API

---
+++ src/api.js
@@ -0,0 +1,10 @@
+//Get les données venant de l'API GeoCities API
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e6ca3b6178msh95803c5c5737682p1e1f3djsn4d6ac5ee4420',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
