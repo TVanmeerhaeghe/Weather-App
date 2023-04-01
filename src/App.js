2db16f3f9a31cdb8202e7309bb79c89eb5427340
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2023-04-01 09:07:34 +0200

    Added forecast for seven days and his style

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import './App.css';
-import Search from './components/search/search';
-import CurrentWeather from './components/current-weather/current-weather';
-import Forecast from './components/forecast/forecast';
-import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
-import { useState } from 'react';
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { useState } from "react";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [forecast, setForecast] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    const [lat, lon]= searchData.value.split("")
+    const [lat, lon] = searchData.value.split("");
 
     //Fetch les données de l'API openweather en récupérant les variables lon et lat
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=fr&units=metric`)
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=fr&units=metric`
+    );
 
     //Fetch les données de l'API openweather pour les prévisions
-    const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=fr&units=metric`)
+    const forecastWeatherFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&lang=fr&units=metric`
+    );
 
-    //Prend en entrée un tableau avec les deux appels APi et les renvoies quand les deux appels sont finis 
+    //Prend en entrée un tableau avec les deux appels APi et les renvoies quand les deux appels sont finis
     Promise.all([currentWeatherFetch, forecastWeatherFetch])
-    .then(async (response) => {
-      //Attend que la promesse soit résolue pour renvoyer les données 
-      const weatherReponse = await response[0].json()
-      const forecastReponse = await response[1].json()
+      .then(async (response) => {
+        //Attend que la promesse soit résolue pour renvoyer les données
+        const weatherReponse = await response[0].json();
+        const forecastReponse = await response[1].json();
 
-      //Set avec les nouvelles données récupéré par l'API ainsi qu'avec le label de la ville 
-      setCurrentWeather({city: searchData.label , ...weatherReponse})
-      setForecast({city: searchData.label , ...forecastReponse})
-    })
-    .catch((err) => console.log(err))
-  }
+        //Set avec les nouvelles données récupéré par l'API ainsi qu'avec le label de la ville
+        setCurrentWeather({ city: searchData.label, ...weatherReponse });
+        setForecast({ city: searchData.label, ...forecastReponse });
+      })
+      .catch((err) => console.log(err));
+  };
 
-  console.log(currentWeather)
-  console.log(forecast)
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {/* Vérifie que la variable existe et si oui renvoie le composant */}
-      {currentWeather && <CurrentWeather data={currentWeather}/>}
-      <Forecast />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,5 +1,91 @@
-const Forecast = () => {
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemPanel,
+  AccordionItemHeading,
+  AccordionItemButton,
+} from "react-accessible-accordion";
 
-}
+import './forecast.css';
 
-export default Forecast
+const WEEK_DAYS = [
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+  "Dimanche",
+];
+
+const Forecast = ({ data }) => {
+  //Récupéré le jour de la semaine
+  const dayInAWeek = new Date().getDay();
+  //Extrait une partie du tableau WEEk_DAYS a partir de la position dayInAWeek qui correspond a la position dans le tableau et jusqu'a la fin du tableau
+  //Puis ajout le nom des jours de la semaine suivante de 0 jusqu'au jour actuel
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat( 
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
+  return (
+    <>
+      <label className="title">Prévisions</label>
+      <Accordion allowZeroExpanded>
+        {/* Limite la récupération des données au 7 prochains jours */}
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="image_temp"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°C /
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pression</label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidité</label>
+                  <label>{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Nuage</label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind</label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Niveau de la mer</label>
+                  <label>{item.main.sealevel}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Ressentie</label>
+                  <label>{Math.round(item.main.feels_like)}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
