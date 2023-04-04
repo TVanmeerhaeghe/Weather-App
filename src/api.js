7b977c2eee86dd95f0f11707f79ad53644afaf98
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2023-04-04 11:09:18 +0200

    Hide my API KEY

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
+
 //Get les données venant de l'API GeoCities API
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "e6ca3b6178msh95803c5c5737682p1e1f3djsn4d6ac5ee4420",
+    "X-RapidAPI-Key": `${process.env.REACT_APP_GEO_API_KEY}`,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -13,4 +14,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 //Export l'url de l'api openweather
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 //Export la key de l'api openweather
-export const WEATHER_API_KEY = "b2aee055d05ceb410aee27c68d26be53";
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
